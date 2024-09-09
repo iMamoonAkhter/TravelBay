75747d0ded80809b44870d7a4c02a8cb7d7e526a
imamoonakhter <imamoonakhter@gmail.com>
2024-09-09 17:45:39 +0500

    Woking on Contact page with emailjs.com

--- server/server.js
+++ server/server.js
@@ -16,6 +16,15 @@ app.use(express.json({
     },
     limit: '50mb',
 }));
+
+app.get('/api/config/emailjs', (req, res)=>{
+    res.send({
+        template_id: process.env.TEMPLATE_ID,
+        service_id: process.env.SERVICE_ID,
+        public_key: process.env.PUBLIC_KEY,
+    })
+})
+
 app.use('/api/blog-posts', BlogPostRoutes)
 const PORT = process.env.PORT || 10000;
 
